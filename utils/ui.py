e0e73997a0d3ecb0d8611e7bc28f0cfef32e12ae
haohuilu <95997507+haohuilu@users.noreply.github.com>
2022-02-02 13:16:43 +1100

    Add files via upload

--- utils/ui.py
+++ utils/ui.py
@@ -16,7 +16,7 @@ from utils.functions import img_to_bytes
 
 
 def introduction():
-    st.title("**Machine Learning in Project Analytics**")
+    st.title("**Welcome to playground for project analytics**")
     st.subheader(
         """
         This is a place where we can get familiar with machine learning models directly from the browser
@@ -27,7 +27,7 @@ def introduction():
         """
     - 🗂️ We use the road construction deley survey as an example (as above). Question 1 - 44 are independent variables (features) and "How often do you experience cost overrun" is dependent variable.
     - ✅ We would like to predict the risk of cost overrun (1 or 2)
-    - ⚙️ Pick a model
+    - ⚙️ Pick a model and set its hyper-parameters
     - 📉 Train it (use 50% of the dataset) and check its performance metrics on test data (50% of the dataset)
     - ✉️ If you have any question, please feel free to email me at haohui.lu@sydney.edu.au
     -----
@@ -43,6 +43,7 @@ def model_selector():
             "Choose a model",
             (
                 "Logistic Regression",
+                "Decision Tree",
                 "Random Forest",
                 "K Nearest Neighbors",
                 "Support Vector Machine"
@@ -51,6 +52,9 @@ def model_selector():
 
         if model_type == "Logistic Regression":
             model = lr_param_selector()
+
+        elif model_type == "Decision Tree":
+            model = dt_param_selector()
 
         elif model_type == "Random Forest":
             model = rf_param_selector()
@@ -77,8 +81,6 @@ def generate_snippet(
     snippet = f"""
     >>> {model_import}
     >>> from sklearn.metrics import accuracy_score, f1_score
-
-
     >>> model.fit(x_train, y_train)
     
     >>> y_train_pred = model.predict(x_train)
