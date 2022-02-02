f195852e93d6109467996ee5c6a2621e5e9a1b06
haohuilu <95997507+haohuilu@users.noreply.github.com>
2022-02-02 12:21:03 +1100

    Add files via upload

--- models/KNearesNeighbors.py
+++ models/KNearesNeighbors.py
@@ -4,12 +4,12 @@ from sklearn.neighbors import KNeighborsClassifier
 
 def knn_param_selector():
 
-    n_neighbors = st.number_input("n_neighbors", 5, 20, 5, 1)
-    metric = st.selectbox(
-        "metric", ("minkowski", "euclidean", "manhattan", "chebyshev", "mahalanobis")
-    )
+    #n_neighbors = st.number_input("n_neighbors", 5, 20, 5, 1)
+    #metric = st.selectbox(
+    #    "metric", ("minkowski", "euclidean", "manhattan", "chebyshev", "mahalanobis")
+    #)
 
-    params = {"n_neighbors": n_neighbors, "metric": metric}
+    #params = {"n_neighbors": n_neighbors, "metric": metric}
 
-    model = KNeighborsClassifier(**params)
+    model = KNeighborsClassifier(metric = 'manhattan', n_neighbors = 3, weights = "distance")
     return model
--- models/SVC.py
+++ models/SVC.py
@@ -4,8 +4,8 @@ from sklearn.svm import SVC
 
 
 def svc_param_selector():
-    C = st.number_input("C", 0.01, 2.0, 1.0, 0.01)
-    kernel = st.selectbox("kernel", ("rbf", "linear", "poly", "sigmoid"))
-    params = {"C": C, "kernel": kernel}
-    model = SVC(**params)
+    #C = st.number_input("C", 0.01, 2.0, 1.0, 0.01)
+    #kernel = st.selectbox("kernel", ("rbf", "linear", "poly", "sigmoid"))
+    #params = {"C": C, "kernel": kernel}
+    model = SVC(C=10, gamma = "scale", kernel = "rbf", random_state = 1)
     return model
